afb67175740545b51aab92509c4d17614b3e1d66
Arnaldo Muñoz Mendoza <kings31lendy@gmail.com>
2020-02-24 22:10:07 -0400

    Repositorio Prueba-Angular

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,10 @@
+export interface InfoPagina {
+  titulo?: string;
+  email?: string;
+  nombre_corto?: string;
+  gitHub?: string;
+  facebook?: string;
+  linkedin?: string;
+  tumblr?: string;
+  equipo_trabajo?: any[];
+}
